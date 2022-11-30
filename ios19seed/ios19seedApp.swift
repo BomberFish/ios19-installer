cf07dcd216451e00a88db5be534d1e00157649b2
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-11-30 10:50:22 -0500

    Initial Commit

---
+++ ios19seed/ios19seedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios19seedApp.swift
+//  ios19seed
+//
+//  Created by Hariz Shirazi on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct ios19seedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios19seedUITests/ios19seedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios19seedUITestsLaunchTests.swift
+//  ios19seedUITests
+//
+//  Created by Hariz Shirazi on 2022-11-30.
+//
+
+import XCTest
+
+final class ios19seedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
